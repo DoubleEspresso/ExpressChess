cf90d94e533417111cc9fbd6b5db1133a9cea64d
Michael Glatzmaier <michael.glatzmaier@gmail.com>
2016-05-28 10:47:42 -0500

    Start to add database support (polyglot hashing, tools for database creations/additions).

---
+++ lib/main.cpp
@@ -0,0 +1,72 @@
+#include "info.h"
+#include "bits.h"
+#include "globals.h"
+#include "magic.h"
+#include "zobrist.h"
+//#include "threads.h"
+
+void parse_args(int argc, char ** argv);
+
+int main(int argc, char ** argv)
+{
+	// simple info/greeting to user
+	Info::BuildInfo::greeting();
+
+	// fill the global chess data arrays for moves/analysis
+	if (Globals::init()) printf("..global arrays initialized\n");
+
+	// the magic bitboard initialization
+	Magic::init();
+	if (!Magic::check_magics())
+	{
+		printf("..!!ERROR : attack hash failed to initialize properly, abort!\n");
+		std::cin.get();
+		return EXIT_FAILURE;
+	}
+	else printf("..attack hash arrays initialized\n");
+
+	// fill the zobrist arrays for transposition table hashing
+	if (!Zobrist::init())
+	{
+		printf("..!!ERROR : failed to initialize zobrist keys, abort!\n");
+		std::cin.get();
+		return EXIT_FAILURE;
+	}
+	else printf("..zobrist keys initialized\n");
+
+
+	// initialize the threadpool (timer thread, master + worker threads)
+	//timer_thread->init();
+	//Threads.init();
+
+	// parse args and start working
+	if (argc > 1) parse_args(argc, argv);
+	else printf("..nothing to do, exiting\n");
+
+	return EXIT_SUCCESS;
+}
+
+// parse user passed arguments here
+void parse_args(int argc, char* argv[])
+{
+  bool dotest = false;
+  int testtime = 0;
+  int testdepth = 0;
+  for (int j = 0; j<argc; ++j)
+    {
+      if (!strcmp(argv[j], "-test"))
+	{
+	}
+      else if (!strcmp(argv[j], "-match")) printf("..match mode\n");
+      else if (!strcmp(argv[j], "-bench")) printf("..benchmark\n");
+      else if (!strcmp(argv[j], "-usett")) printf("..set option tt\n");
+      else if (!strcmp(argv[j], "-ttsizekb")) printf("..set option ttsizekb\n");
+      else if (!strcmp(argv[j], "-nthreads")) printf("..set option nthreads\n");
+      else if (!strcmp(argv[j], "-trace")) printf("..set option eval trace\n");
+      else if (!strcmp(argv[j], "-stats")) printf("..set option stats\n");
+      else if (!strcmp(argv[j], "-log")) printf("..set option log\n");
+      else if (!strcmp(argv[j], "-testtime")) { testtime = atoi(argv[j + 1]); j++; if (j >= argc) break; }
+      else if (!strcmp(argv[j], "-testdepth")) { testdepth = atoi(argv[j + 1]); j++; if (j >= argc) break; }
+    }
+}
+
